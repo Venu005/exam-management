2d3b62ccc3e45551b6e6c0d3561ffa487dbc247e
Venu005 <venusaiyalamanchili@gmail.com>
2025-01-08 11:52:57 +0530

    chore: auth complete

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://allowed-grub-5.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,47 @@
+"use client";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+} from "@clerk/nextjs";
+import React from "react";
+import { Button } from "./ui/button";
+import { LogInIcon, LogOutIcon } from "lucide-react";
+
+export const NavBar = () => {
+  return (
+    <nav className="flex items-end justify-between py-4 px-6 bg-background border-b">
+      hello
+      <div className="flex items-center ml-auto">
+        <SignedIn>
+          <SignOutButton>
+            <Button
+              variant={"outline"}
+              size={"sm"}
+              className="flex items-center gap-2"
+            >
+              <LogOutIcon className="size-4" />
+              <span className="hidden sm:inline">Logout</span>
+            </Button>
+          </SignOutButton>
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button
+              variant={"outline"}
+              size={"sm"}
+              className="flex items-center gap-2"
+            >
+              <LogInIcon className="size-4" />
+              <span className="hidden sm:inline">Login</span>
+            </Button>
+          </SignInButton>
+        </SignedOut>
+        <UserButton />
+      </div>
+    </nav>
+  );
+};
